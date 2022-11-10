fe438ca7d59f7624137218e493164438cf263ab0
Nicolas Alvez <nicoalvez99@gmail.com>
2022-11-10 15:26:13 -0300

    Cambios

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const contraseña = prompt("Ingrese la contraseña");
-if(contraseña == "holy.admin"){
-    window.onload;
-} else {
-    window.close();
-}
+
+let boton = document.getElementById('boton');
+
+boton.addEventListener("click", ()=>{
+    let pass = document.getElementById('pass').value;
+    if(pass == "holy.admin"){
+        window.location.href = "https://holywins.github.io/documento-de-bienvenida/";
+    } else {
+        alert("Contraseña Incorrecta.");
+    }
+});
+
+
