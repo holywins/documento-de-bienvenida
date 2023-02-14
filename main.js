ee76cb393065c3dafde859a8db327859e30245b0
Nicolas Alvez <nicoalvez99@gmail.com>
2023-02-14 14:04:43 -0300

    Camibios

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-const contraseña = prompt("Ingrese la contraseña");
-if(contraseña == "holy.admin"){
-    window.onload;
-} else {
-    window.close();
-}
+let luna = document.getElementById('luna');
+let sol = document.getElementById('sol');
+
+luna.addEventListener("click", ()=> {
+    let body = document.getElementById('body')
+    let parrafo = document.getElementById('parrafo');
+    siExiste = body.classList.contains("bodyWhite")
+    siTambienExiste = parrafo.classList.contains("parrafoWhite");
+    if (siExiste && siTambienExiste){
+        body.classList.remove("bodyWhite");
+        parrafo.classList.remove("parrafoWhite");
+    }
+    body.classList.add("bodyBlack");
+    parrafo.classList.add("parrafoBlack");
+});
+
+sol.addEventListener("click", ()=>{
+    let body = document.getElementById('body')
+    let parrafo = document.getElementById('parrafo');
+    body.classList.remove("bodyBlack");
+    parrafo.classList.remove("parrafoBlack");
+    body.classList.add("bodyWhite");
+    parrafo.classList.add("parrafoWhite");
+})
